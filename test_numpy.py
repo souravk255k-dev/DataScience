52212ac240a8478f8de78ca699a68c147de7e663
souravk255k-dev <souravk255k@gmail.com>
2025-08-23 14:04:26 +0530

    Create test_numpy.py

---
+++ test_numpy.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+# a = np.array([1, 2, 3, 4, 5])
+# print("Array a:")
+# print(a)
+
+# b = np.zeros((2, 3))
+# print("\nArray b (2x3 zeros):")
+# print(b)
+
+# c = np.ones((2, 2))
+# print("\nArray c (2x2 ones):")
+# print(c)
+
+# d = np.arange(0, 10, 2)
+# print("\nArray d (arange from 0 to 10, step 2):")
+# print(d)
+
+# e = np.arange(6)
+# print("\nArray e:")
+# print(e)
+
+# f = e.reshape((2, 3))
+# print("\nReshaped e to 2x3:")
+# print(f)
+
+# arr = np.array([1, 2, 3, 4])
+# print("\nMean of arr:", np.mean(arr))
+# print("Median of arr:", np.median(arr))
+# print("Standard Deviation of arr:", np.std(arr))
+
+# a=np.random.rand()
+# print(a)
+# b=np.random.rand(5)
+# print(b)
+# c=np.random.rand(2,3)
+# print(c)
+
+# x=np.random.randint(1,10)
+# print(x)
+# y=np.random.randint(0,100,size=5)
+# print(y)
+# z=np.random.randint(0,100,size=5)
+# print(z)
+
+
+otp = np.random.randint(100000, 1000000)  
+print(otp)
+
