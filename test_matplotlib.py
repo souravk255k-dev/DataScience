a00e42dc7b92ac0c42865847e6636ba666f00012
souravk255k-dev <souravk255k@gmail.com>
2025-08-27 16:59:47 +0530

    Update test_matplotlib.py

--- test_matplotlib.py
+++ test_matplotlib.py
@@ -11,8 +11,42 @@ import pandas as pd
 # plt.show()
 
 
-x=np.random.rand(50)
-y=np.random.rand(50)
-plt.scatter(x,y,color='blue',marker='o')
-plt.title("Scatter plot")
-plt.show()
+# x=np.random.rand(50)
+# y=np.random.rand(50)
+# plt.scatter(x,y,color='blue',marker='o')
+# plt.title("Scatter plot")
+# plt.show()
+
+# categories=['A','B','C']
+# values=[3,7,5]
+# plt.bar(categories,values,color='purple')
+# plt.title("Bar plot")
+# plt.show()
+
+# data=np.random.randn(1000)
+# plt.hist(data,bins=30,color='skyblue',edgecolor='black')
+# plt.title("histogram")
+# plt.show()
+
+# sizes = [20, 30, 25, 25]
+# labels = ['A', 'B', 'C', 'D']
+
+# plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+# plt.title("Pie Chart")
+# plt.show()
+
+# days=[1,2,3,4,5]
+# sleeping=[7,8,6,11,7]
+# eating=[2,3,4,3,2]
+# working=[7,8,7,2,2]
+# playing=[8,5,7,8,13]
+# plt.stackplot(days,sleeping,eating,working,playing,labels=['sleep','Eat','work','play'])
+# plt.legend(loc='upper left')
+# plt.title("stacked Area plot")
+# plt.show()
+
+# data=np.random.normal(100,20,200)
+# plt.boxplot(data)
+# plt.title("Box plot")
+# plt.show()
+
